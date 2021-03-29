a20208dedad66a72715488db0fa2cb644f0ecb5a
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2021-03-28 22:38:03 -0400

    first commit

---
+++ tldprobe.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys, subprocess, datetime
+
+base = sys.argv[1]
+
+with open('output/combinations-{}.txt'.format(base), 'a') as output_file:
+	cctlds = gtlds = []
+	with open('cctlds.txt', 'r') as cctlds_file:
+		cctlds = cctlds_file.read().splitlines()
+	with open('short_gtlds.txt', 'r') as gtlds_file:
+		gtlds = gtlds_file.read().splitlines()
+	for cctld in cctlds:
+		for gtld in gtlds:
+			output_file.write("{}.{}.{}\n".format(base, gtld, cctld))
+	with open('tlds.txt', 'r') as tlds_file:
+		for tld in tlds_file:
+			output_file.write("{}.{}".format(base, tld))
+subprocess.call(['httpx', '-l', 'output/combinations-{}.txt'.format(base), '-location', '-content-length', '-status-code', '-title', '-silent', '-ports', '80,443,8008,8080,8443', '-o', 'http-{}.txt'.format(base)])
